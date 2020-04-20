ca3a5b538917a366442d1b76d36b5546e2a4dc37
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2020-04-20 11:32:07 +0100

    Tidy up

--- single-sng_post.php
+++ single-sng_post.php
@@ -5,15 +5,10 @@
 <?php get_template_part('template-part', 'topnav'); ?>
  
 <!-- start content container -->
-<?php //get_template_part( 'content', 'single' ); 
-get_template_part( 'template-parts/content', get_post_type() );
-?>
+<?php get_template_part( 'template-parts/content', get_post_type() ); ?>
 <!-- end content container -->
 
 <?php get_template_part('template-part', 'footernav'); ?>
 
 <?php get_footer(); ?>
 <?php get_footer(); ?>
-
-<?php
-//get_template_part( 'template-parts/content', get_post_type() );
