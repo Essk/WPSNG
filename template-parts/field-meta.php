51fd633ac847f39a1f122d1494f2b8b246b93db7
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2020-04-20 14:31:59 +0100

    Conditionally render links in meta group

--- template-parts/field-meta.php
+++ template-parts/field-meta.php
@@ -4,8 +4,28 @@
         ?>
             <aside> 
                 <ul>
-                <?php foreach($fields as $key => $value): ?>
-                    <li><strong><?php echo($key)?></strong>: <?php echo($value)?> </li>
+                <?php foreach($fields as $key => $value): 
+                    $field = $fields[$key];
+                    ?>
+                    <li><strong><?php echo($key)?></strong>: 
+                    <?php if(
+                        is_array($field) 
+                        && array_key_exists('text', $field) 
+                        && array_key_exists('url', $field)): 
+                        $text = $fields[$key]['text'];
+                        $url = $fields[$key]['url'];
+                        $link_text = $text && $text !== '' ? $text : $url;
+                        if($url && $url !== ''): ?>
+                            <a href="<?php echo $url?>">
+                                <?php echo $link_text; ?>
+                            </a>
+                        <?php else: ?>
+                        <?php echo $text; ?>
+                        <?php endif; ?>
+                    <?php else: ?>
+                        <?php echo $value; ?>
+                    <?php endif; ?>
+                    </li>
                 <?php endforeach; ?>    
                 </ul>
             </aside>
